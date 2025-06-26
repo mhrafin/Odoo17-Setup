4038410035b5a1efab013abcb0ba8a049c8e4705
rafhome <110781737+mhrafin@users.noreply.github.com>
2025-06-26 10:00:45 +0600

    Script tested line by line. Working. Need to test in a single go.

--- odoo-server.sh
+++ odoo-server.sh
@@ -1,228 +1,76 @@
 #!/bin/bash
-################################################################################
-# Script for installing Odoo on Ubuntu 24.04
-# Author: Mahedi Hassan Rafin
-#-------------------------------------------------------------------------------
-# This script will install Odoo on your Ubuntu server. It can install multiple Odoo instances
-# in one Ubuntu because of the different xmlrpc_ports
-#-------------------------------------------------------------------------------
-# Make a new file:
-# sudo nano odoo-server.sh
-# Place this content in it and then make the file executable:
-# sudo chmod +x odoo-server.sh
-# Execute the script to install Odoo:
-# ./odoo-server
-################################################################################
 
-OE_USER="odoo"
-OE_HOME="/$OE_USER"
-OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
-# The default port where this Odoo instance will run under (provided you use the command -c in the terminal)
-# Set to true if you want to install it, false if you don't need it or have it already installed.
-INSTALL_WKHTMLTOPDF="True"
-# Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8069"
-# Choose the Odoo version which you want to install. For example: 16.0, 15.0, 14.0 or saas-22. When using 'master' the master version will be installed.
-# IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 17.0
-OE_VERSION="17.0"
+# Updating Ubuntu and Installing the First Set of Required Packages
+sudo apt update
+sudo apt upgrade -y
 
+sudo apt install postgresql postgresql-client build-essential python3-pillow python3-lxml python3-dev python3-pip python3-setuptools npm nodejs git gdebi libldap2-dev libsasl2-dev libxml2-dev python3-wheel python3-venv libxslt1-dev node-less libjpeg-dev libpq-dev -y
 
-# Set this to True if you want to install Nginx!
-INSTALL_NGINX="False"
-# Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
-OE_SUPERADMIN="admin"
-# Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
-GENERATE_RANDOM_PASSWORD="True"
-OE_CONFIG="${OE_USER}-server"
-# Set the website name
-WEBSITE_NAME="_"
-# Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-LONGPOLLING_PORT="8072"
-# Set the default workers. Rule of thumb : workers = (CPU * 2) + 1
-WORKERS=4
-# Set to "True" to install certbot and have ssl enabled, "False" to use http
-ENABLE_SSL="True"
-# Provide Email to register ssl certificate
-ADMIN_EMAIL="odoo@example.com"
-##
-###  WKHTMLTOPDF download links
-## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
-## in order to have correct version of wkhtmltopdf installed, for a danger note refer to
-## https://github.com/odoo/odoo/wiki/Wkhtmltopdf ):
-## https://www.odoo.com/documentation/17.0/administration/install.html
-
-WKHTMLTOX_X64="https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb"
-
-
-#--------------------------------------------------
-# Update Server
-#--------------------------------------------------
-echo -e "\n---- Update Server ----"
-
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install libpq-dev -y
-
-#--------------------------------------------------
-# Install PostgreSQL Server
-#--------------------------------------------------
-echo -e "\n---- Install PostgreSQL V16 Server ----"
-
-sudo curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-sudo apt-get update
-sudo apt-get install postgresql-16 -y
-
-
-
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
-
-#--------------------------------------------------
-# Install Dependencies
-#--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install python3 python3-pip
-sudo apt-get install git python3-cffi build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng-dev libjpeg-dev gdebi -y
-
-echo -e "\n---- Creating Python virtual environment ----"
-python3 -m venv ${OE_HOME}/venv
-sudo chown -R $OE_USER:$OE_USER ${OE_HOME}/venv
-
-echo -e "\n---- Activating virtual environment and installing requirements ----"
-${OE_HOME}/venv/bin/pip install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
-
-
-echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
-sudo apt-get install nodejs npm -y
+# For languages using a right-to-left interface (such as Arabic or Hebrew), the rtlcss package is required.
 sudo npm install -g rtlcss
 
-#--------------------------------------------------
-# Install Wkhtmltopdf if needed
-#--------------------------------------------------
-if [ "$INSTALL_WKHTMLTOPDF" = "True" ]; then
-  echo -e "\n---- Installing wkhtmltopdf ----"
+# Creating a System User
+sudo useradd -m -d /opt/odoo17 -U -r -s /bin/bash odoo17
 
-  wget $WKHTMLTOX_X64
-  sudo apt-get install ./$(basename $WKHTMLTOX_X64) -y
+# Configuring PostgreSQL
+sudo su - postgres -c "createuser -s odoo17"
 
-  sudo ln -sf /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
-  sudo ln -sf /usr/local/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
+# Installing Wkhtmltopdf
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.jammy_amd64.deb
 
-else
-  echo "Wkhtmltopdf is not installed due to the user's choice."
-fi
+sudo apt install ./wkhtmltox_0.12.6.1-3.jammy_amd64.deb -y
 
 
-echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
-#The user should also be added to the sudo'ers group.
-sudo adduser $OE_USER sudo #Skip this line for tighter security.
+# Downloading, Installing, and Configuring Odoo 17 in a Virtual Environment
+sudo -u odoo17 -H git clone https://www.github.com/odoo/odoo --depth 1 --branch 17.0 /opt/odoo17/odoo
+sudo -u odoo17 -H python3 -m venv /opt/odoo17/odoo-venv
 
-# echo -e "\n---- Create Log directory ----"
-# sudo mkdir /var/log/$OE_USER
-# sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
+# Install the Required Odoo Python dependencies
+sudo -u odoo17 -H /opt/odoo17/odoo-venv/bin/pip install wheel
+sudo -u odoo17 -H /opt/odoo17/odoo-venv/bin/pip install -r /opt/odoo17/odoo/requirements.txt
 
-#--------------------------------------------------
-# Install ODOO
-#--------------------------------------------------
-echo -e "\n==== Installing ODOO Server ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+# Creating a configuration file for our Odoo installation
+sudo tee /etc/odoo17.conf > /dev/null <<EOF
+[options]
+; Specify the password that allows database management:
+admin_passwd = scrpass
+db_host = False
+db_port = False
+db_user = odoo17
+db_password = False
+addons_path = /opt/odoo17/odoo/addons
+; This is the default port. It is specified here as you will want to set this if you are running Odoo on an alternate port.
+xmlrpc_port = 8069
+; This is the default longpolling port. Like the xmlrpc_port we are specifying this port for completeness
+longpolling_port = 8072
+; If you plan on setting up nginx it is advised to specify multiple workers in the configuration. If you don’t set this to workers > 1 then you could run into problems when you specify the long polling blocks in the nginx config file.
+workers = 2
+; You will want to add a dbfilter to your config file if you have more than one database. The ; means the command is commented out. Remove the ; and specify the database so that your Odoo installation knows exactly which database to use for the instance.
+; dbfilter = [your database]
+EOF
 
-
-echo -e "\n---- Create custom module directory ----"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
-
-echo -e "\n---- Setting permissions on home folder ----"
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
-
-echo -e "* Create server config file"
-
-sudo touch /etc/${OE_CONFIG}.conf
-echo -e "* Creating server config file"
-sudo su root -c "printf '[options] \n; This is the password that allows database operations:\n' >> /etc/${OE_CONFIG}.conf"
-if [ "$GENERATE_RANDOM_PASSWORD" = "True" ]; then
-    echo -e "* Generating random admin password"
-    OE_SUPERADMIN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
-fi
-sudo su root -c "printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> /etc/${OE_CONFIG}.conf"
-
-# Use False for default local PostgreSQL peer-auth
-sudo su root -c "printf 'db_host = False\n' >> /etc/${OE_CONFIG}.conf"
-sudo su root -c "printf 'db_port = False\n' >> /etc/${OE_CONFIG}.conf"
-sudo su root -c "printf 'db_user = ${OE_USER}\n' >> /etc/${OE_CONFIG}.conf"
-sudo su root -c "printf 'db_password = False\n' >> /etc/${OE_CONFIG}.conf"
-
-sudo su root -c "printf 'http_port = ${OE_PORT}\n' >> /etc/${OE_CONFIG}.conf"
-
-sudo su root -c "printf 'longpolling_port = ${LONGPOLLING_PORT}\n' >> /etc/${OE_CONFIG}.conf"
-
-# sudo su root -c "printf 'logfile = /var/log/${OE_USER}/${OE_CONFIG}.log\n' >> /etc/${OE_CONFIG}.conf"
-
-sudo su root -c "printf 'addons_path=${OE_HOME_EXT}/addons,${OE_HOME}/custom/addons\n' >> /etc/${OE_CONFIG}.conf"
-
-sudo su root -c "printf 'workers = ${WORKERS}\n' >> /etc/${OE_CONFIG}.conf"
-
-sudo chown $OE_USER:$OE_USER /etc/${OE_CONFIG}.conf
-sudo chmod 640 /etc/${OE_CONFIG}.conf
-
-
-
-#--------------------------------------------------
-# Create systemd service for Odoo
-#--------------------------------------------------
-
-echo -e "* Creating Odoo systemd service file"
-
-sudo tee /etc/systemd/system/odoo.service > /dev/null <<EOF
+# Creating a Systemd unit file to auto-start Odoo when the server reboots
+sudo tee /etc/systemd/system/odoo17.service > /dev/null <<EOF
 [Unit]
-Description=Odoo ERP Server
+Description=Odoo17
 Requires=postgresql.service
 After=network.target postgresql.service
 
 [Service]
 Type=simple
-User=${OE_USER}
-Group=${OE_USER}
-ExecStart=${OE_HOME}/venv/bin/python3 ${OE_HOME_EXT}/odoo-bin -c /etc/${OE_CONFIG}.conf
+SyslogIdentifier=odoo17
+PermissionsStartOnly=true
+User=odoo17
+Group=odoo17
+ExecStart=/opt/odoo17/odoo-venv/bin/python3 /opt/odoo17/odoo/odoo-bin -c /etc/odoo17.conf
 StandardOutput=journal+console
-Restart=always
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-
-
-
-
-
-# sudo systemctl start odoo     # start the server
-# sudo systemctl stop odoo      # stop it
-# sudo systemctl restart odoo   # restart
-# sudo systemctl status odoo    # view logs/status
-
-
-
-
-echo -e "* Starting Odoo Service"
-sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
-sudo systemctl enable odoo
-sudo systemctl start odoo
-echo "-----------------------------------------------------------"
-echo "Done! The Odoo server is up and running. Specifications:"
-echo "Port: $OE_PORT"
-echo "User service: $OE_USER"
-echo "Configuraton file location: /etc/${OE_CONFIG}.conf"
-# echo "Logfile location: /var/log/$OE_USER"
-echo "User PostgreSQL: $OE_USER"
-echo "Code location: $OE_USER"
-echo "Addons folder: $OE_USER/$OE_CONFIG/addons/"
-echo "Password superadmin (database): $OE_SUPERADMIN"
 
-if [ $INSTALL_NGINX = "True" ]; then
-  echo "Nginx configuration file: /etc/nginx/sites-available/$WEBSITE_NAME"
-fi
-echo "-----------------------------------------------------------"
+sudo systemctl enable --now odoo17
+
+sudo systemctl status odoo17
